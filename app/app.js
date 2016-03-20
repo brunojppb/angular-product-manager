f175a93d3ac7c70d72a106a8eb394f853430e5ab
Bruno Paulino <brunojppb@gmail.com>
2016-03-20 17:27:21 -0300

    Adding UI Router

--- app/app.js
+++ app/app.js
@@ -1,6 +1,21 @@
-(function () {
-    "use strict";
-    var app = angular.module("productManagement",
-                            ["common.services",
-                             "productResourceMock"]);
+(function() {
+  "use strict";
+  var app = angular.module("productManagement", ["common.services",
+    "ui.router",
+    "productResourceMock"
+  ]);
+
+
+  app.config(['$stateProvider',
+      function($stateProvider) {
+        $stateProvider
+          .state("productList",{
+            url: '/products',
+            templateUrl: 'app/products/productListView.html',
+            controller: 'ProductListCtrl as vm'
+          })
+      }
+    ]
+
+  );
 }());
