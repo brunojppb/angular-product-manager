03ab5e4dcaa2256e762e8267e6a3512446c33596
bruno paulino <brunojppb@gmail.com>
2016-03-21 11:59:13 -0300

    presenting analytics with Charts

--- app/app.js
+++ app/app.js
@@ -1,79 +1,89 @@
 (function() {
   "use strict";
   var app = angular.module("productManagement", ["common.services",
-    "ui.router", "ui.mask", "ngAnimate", "ngTouch", "ui.bootstrap", "toastr", "ngMessages",
+    "ui.router",
+    "ui.mask",
+    "ngAnimate",
+    "ngTouch",
+    "ui.bootstrap",
+    "toastr",
+    "ngMessages",
+    "angularCharts",
     "productResourceMock"
   ]);
 
 
-  app.config(["$stateProvider", "$urlRouterProvider",
-      function($stateProvider, $urlRouterProvider) {
+  app.config(["$stateProvider",
+    "$urlRouterProvider",
+    function($stateProvider, $urlRouterProvider) {
+      $urlRouterProvider.otherwise("/");
 
-        // Default url
-        // It will redirect the default route
-        // To this
-        $urlRouterProvider.otherwise("/");
+      $stateProvider
+        .state("home", {
+          url: "/",
+          templateUrl: "app/welcomeView.html"
+        })
+        // Products
+        .state("productList", {
+          url: "/products",
+          templateUrl: "app/products/productListView.html",
+          controller: "ProductListCtrl as vm"
+        })
+        .state("productEdit", {
+          abstract: true,
+          url: "/products/edit/:productId",
+          templateUrl: "app/products/productEditView.html",
+          controller: "ProductEditCtrl as vm",
+          resolve: {
+            productResource: "productResource",
 
-        $stateProvider
-          // Home
-          .state("home",{
-            url: "/",
-            templateUrl: "app/welcomeView.html"
-          })
-          // Products
-          .state("productList", {
-            url: "/products",
-            templateUrl: "app/products/productListView.html",
-            controller: "ProductListCtrl as vm"
-          })
-
-
-
-          .state("productEdit", {
-            abstract: true,
-            url: "/products/edit/:productId",
-            templateUrl: "app/products/productEditView.html",
-            controller: "ProductEditCtrl as vm",
-            // Retrive data and inject in the controller
-            resolve: {
-              productResource: "productResource",
-              product: function(productResource, $stateParams) {
-                var productId = $stateParams.productId;
-                return productResource.get(
-                  {productId: productId}).$promise;
-              }
+            product: function(productResource, $stateParams) {
+              var productId = $stateParams.productId;
+              return productResource.get({
+                productId: productId
+              }).$promise;
             }
-          })
-          .state("productEdit.info", {
-            url: "/info",
-            templateUrl: "app/products/productEditInfoView.html"
-          })
-          .state("productEdit.price", {
-            url: "/price",
-            templateUrl: "app/products/productEditPriceView.html"
-          })
-          .state("productEdit.tags", {
-            url: "/tags",
-            templateUrl: "app/products/productEditTagsView.html"
-          })
+          }
+        })
+        .state("productEdit.info", {
+          url: "/info",
+          templateUrl: "app/products/productEditInfoView.html"
+        })
+        .state("productEdit.price", {
+          url: "/price",
+          templateUrl: "app/products/productEditPriceView.html"
+        })
+        .state("productEdit.tags", {
+          url: "/tags",
+          templateUrl: "app/products/productEditTagsView.html"
+        })
+        .state("productDetail", {
+          url: "/products/:productId",
+          templateUrl: "app/products/productDetailView.html",
+          controller: "ProductDetailCtrl as vm",
+          resolve: {
+            productResource: "productResource",
 
-
-          .state("productDetail", {
-            url: "/products/:productId",
-            templateUrl: "app/products/productDetailView.html",
-            controller: "ProductDetailCtrl as vm",
-            // Retrive data and inject in the controller
-            resolve: {
-              productResource: "productResource",
-              product: function(productResource, $stateParams) {
-                var productId = $stateParams.productId;
-                return productResource.get(
-                  {productId: productId}).$promise;
-              }
+            product: function(productResource, $stateParams) {
+              var productId = $stateParams.productId;
+              return productResource.get({
+                productId: productId
+              }).$promise;
             }
-          });
-      }
-    ]
+          }
+        })
+        .state("priceAnalytics", {
+          url: "/price-analytics",
+          templateUrl: "app/prices/priceAnalyticsView.html",
+          controller: "PriceAnalyticsCtrl",
+          resolve: {
+            productResource: "productResource",
 
-  );
+            products: function(productResource) {
+              return productResource.query().$promise;
+            }
+          }
+        })
+    }
+  ]);
 }());
